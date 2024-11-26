95b60143356336c46d8cdc95e219d79c8286176b
Alexander Petyukov <sandrounknown1991@gmail.com>
2024-11-26 16:53:58 +0300

    init commit

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -10,7 +10,7 @@ import java.net.URL;
 public interface WebDriverConfig extends Config {
 
     @Key("browser.name")
-    @DefaultValue("CHROME")
+    @DefaultValue("chrome")
     String getBrowserName();
 
     @Key("browser.version")
